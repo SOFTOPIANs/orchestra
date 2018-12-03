a5ef16dce78d669e5a2286e108c234d738549eb3
Alessandro Di Federico <ale@clearmind.me>
2018-12-03 16:41:17 +0100

    CI: use a script to install the dependencies
    We now have a script to install dependencies on top of the
    `revng/revng-build-dependencies` docker image. This is useful to be able
    to install in the CI component that are not definitive dependencies yet,
    without having to update the docker image each time.

---
+++ support/install-dependencies.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+
+apt-get install --no-install-recommends --yes \
+  aufs-tools \
+  build-essential \
+  ca-certificates \
+  cmake \
+  curl \
+  gawk \
+  git \
+  libglib2.0-dev \
+  libgmp-dev \
+  libmpc-dev \
+  libmpfr-dev \
+  pkg-config \
+  python \
+  python-cffi \
+  python-pyelftools \
+  python-pygraphviz \
+  python-setuptools \
+  sed \
+  texinfo \
+  valgrind \
+  zlib1g-dev
+
+if ! which git-lfs >& /dev/null; then
+  curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+  apt-get install --no-install-recommends --yes git-lfs
+fi
